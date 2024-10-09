db58a08ac8bd694ec5b9168b6f06c0808550b7a4
MochamadLukyAdithia <165120779+MochamadLukyAdithia@users.noreply.github.com>
2024-10-09 14:08:17 +0700

    add all

---
+++ app/src/main/java/com/millenialzdev/myloginandregistersqlite/Lupa.java
@@ -0,0 +1,19 @@
+package com.millenialzdev.myloginandregistersqlite;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Lupa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lupa);
+
+    }
+}
